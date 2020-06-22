af2ed3f16dcb0ab197c3fe7b674e1ef657c0a45c
lakshaymadhav <lakshaymadhav25@gmail.com>
2020-06-22 17:41:42 +0530

    changes done

---
+++ lib/homepage.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+import 'package:Covid19_Tracker/widgets/Indiapanel.dart';
+import 'package:flutter/material.dart';
+import 'datasoruce.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  Map IndiaData;
+  fetchIndiaData()async{
+    http.Response response=await http.get('https://api.rootnet.in/covid19-in/stats/latest');
+    setState(() {
+      IndiaData=json.decode(response.body);
+    });
+  }
+
+@override
+  void initState() {
+    // TODO: implement initState
+    fetchIndiaData();
+    super.initState();
+
+  }
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('COIVD-19 TRACKER APP'),
+      ),
+      body: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 75,
+              
+              alignment: Alignment.center,
+              padding:EdgeInsets.all(10) ,
+              color: Colors.orange[100],
+              child: Text(
+                DataSource.quote,
+                style: TextStyle(color: Colors.orange[800],
+                fontWeight: FontWeight.bold,
+                fontSize: 16 ,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical:10.0,horizontal: 10.0),
+              child: Text("INDIA",style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
+            ),
+            IndiaData==null?CircularProgressIndicator():IndiaPanel(IndiaData: IndiaData,), 
+          ],
+        ),
+      ) ,
+    );
+  }
+}
---
+++ lib/widgets/Indiapanel.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class IndiaPanel extends StatelessWidget {
+
+  final Map IndiaData;
+
+  const IndiaPanel({Key key, this.IndiaData}) : super(key: key);
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GridView(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 2
+          ),
+          children: <Widget>[
+            StatusPanel(
+              title: "CONFIRMED",
+              PanelColor: Colors.red[100],
+              TextColor: Colors.red,
+              counter: IndiaData['total'].toString(),
+            ),
+            StatusPanel(
+               title: "ACTIVE",
+              PanelColor: Colors.blue[100],
+              TextColor: Colors.blue[900],
+              counter: IndiaData[''].toString(),
+            ),
+            StatusPanel(
+               title: "RECOVERED",
+              PanelColor: Colors.green[100],
+              TextColor: Colors.green,
+              counter: IndiaData['Cured/Discharged/Migrated'].toString(),
+            ),
+            StatusPanel(
+               title: "DEATHS",
+              PanelColor: Colors.grey[400],
+              TextColor: Colors.grey[900],
+              counter: IndiaData['Death'].toString(),
+            ),
+          ],
+      ),
+    );
+  }
+}
+
+class StatusPanel extends StatelessWidget {
+
+
+  final Color PanelColor;
+  final Color TextColor;
+  final String title;
+  final String counter;
+
+  const StatusPanel({Key key, this.PanelColor, this.TextColor, this.title, this.counter}) : super(key: key);
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return Container(
+      margin: EdgeInsets.all(10),
+      color: PanelColor,
+      height: 80,
+      width: width/2,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(title,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16,color: TextColor),),
+          Text(counter,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16,color: TextColor),),     
+             ],
+      ),
+    );
+  }
+}
