d21cb6b0d525bac7741dac94c11aeb0d30519788
lakshaymadhav <lakshaymadhav25@gmail.com>
2020-06-24 19:46:59 +0530

    added pull to refresh

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -27,7 +27,7 @@ class _HomePageState extends State<HomePage> {
   List StateData;
   fetchStateData() async {
     http.Response response =
-        await http.get('https://api.covidindiatracker.com/state_data.json?sort=confirmed');
+        await http.get('https://api.covidindiatracker.com/state_data.json');
     setState(() {
       StateData = json.decode(response.body);
     });
@@ -41,83 +41,91 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  Future<Null> refreshList() async{
+    await http.get('https://api.covidindiatracker.com/total.json');
+    await http.get('https://api.covidindiatracker.com/state_data.json');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return RefreshIndicator(
+      onRefresh: refreshList,
+          child: Scaffold(
+        appBar: AppBar(
 
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Theme.of(context).brightness==Brightness.light?Icons.lightbulb_outline:Icons.highlight),
-             onPressed: (){
-               DynamicTheme.of(context).setBrightness(Theme.of(context).brightness==Brightness.light?Brightness.dark:Brightness.light);
-             }),
-        ],
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Theme.of(context).brightness==Brightness.light?Icons.lightbulb_outline:Icons.highlight),
+               onPressed: (){
+                 DynamicTheme.of(context).setBrightness(Theme.of(context).brightness==Brightness.light?Brightness.dark:Brightness.light);
+               }),
+          ],
 
-        centerTitle: false,
-        title: Text('COIVD-19 TRACKER APP'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              height: 75,
-              alignment: Alignment.center,
-              padding: EdgeInsets.all(10),
-              color: Colors.orange[100],
-              child: Text(
-                DataSource.quote,
-                style: TextStyle(
-                  color: Colors.orange[800],
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
+          centerTitle: false,
+          title: Text('COIVD-19 TRACKER APP'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                height: 75,
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(10),
+                color: Colors.orange[100],
+                child: Text(
+                  DataSource.quote,
+                  style: TextStyle(
+                    color: Colors.orange[800],
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children:<Widget>[ 
-                Text("INDIA",style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
-                
-                GestureDetector(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>StatePage()));
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        color: primaryBlack,
-                        borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Text("Regional",style: TextStyle(fontSize: 16,color: Colors.white, fontWeight: FontWeight.bold),)
-                    
-                    ),
-                ),
-                
-                  ],
-                ),
-            ),
-            IndiaData == null? CircularProgressIndicator(): IndiaPanel(
-                    IndiaData: IndiaData,
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:<Widget>[ 
+                  Text("INDIA",style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+                  
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>StatePage()));
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: primaryBlack,
+                          borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Text("Regional",style: TextStyle(fontSize: 16,color: Colors.white, fontWeight: FontWeight.bold),)
+                      
+                      ),
                   ),
-            
-             Padding( 
-               padding: const EdgeInsets.symmetric(horizontal:10.0),
-               child: Text("Most Affected States",style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
-             ),
+                  
+                    ],
+                  ),
+              ),
+              IndiaData == null? CircularProgressIndicator(): IndiaPanel(
+                      IndiaData: IndiaData,
+                    ),
+              
+               Padding( 
+                 padding: const EdgeInsets.symmetric(horizontal:10.0),
+                 child: Text("Most Affected States",style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+               ),
 
-            SizedBox(height:10),
+              SizedBox(height:10),
 
-             StateData==null?Container():MostAffectedStates(StateData: StateData,),
-             InfoPanel(),
-             SizedBox(height: 20,),
-             Center(child: Text('STAY HOME, STAY SAFE',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),)),
-             SizedBox(height: 50,),
-          ],
+               StateData==null?Container():MostAffectedStates(StateData: StateData,),
+               InfoPanel(),
+               SizedBox(height: 20,),
+               Center(child: Text('STAY HOME, STAY SAFE',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),)),
+               SizedBox(height: 50,),
+            ],
+          ),
         ),
       ),
     );
